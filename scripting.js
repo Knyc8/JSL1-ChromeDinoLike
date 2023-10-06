15fe2de17fd4994acc54f838aef59fa098ad801a
knyc8 <kchen9712@bths.edu>
2023-10-06 15:31:27 -0400

    added player sprite and basic jump code

--- scripting.js
+++ scripting.js
@@ -1,2 +1,13 @@
 const player = document.getElementById("player");
 const obs = document.getElementById("obstacle");
+const jumpbtn = document.getElementById("jumpbtn");
+
+function jump() {
+    player.classList.add("jump");
+
+    setTimeout(function () {
+        player.classList.remove("jump");
+    }, 750);
+}
+
+jumpbtn.addEventListener("click", jump);
